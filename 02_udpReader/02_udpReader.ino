97e36238a9c90220172301098342c3bfd427d2ab
woeetech <vlukas@hotmail.cz>
2020-10-11 19:01:47 +0200

    working direct recieveing, broadcast still WIP

--- 02_udpReader/02_udpReader.ino
+++ 02_udpReader/02_udpReader.ino
@@ -13,13 +13,21 @@ IPAddress myDns(192, 168, 2, 1);
 IPAddress gateway(192, 168, 2, 1);
 IPAddress subnet(255, 255, 255,0);
 
+//ports around 50000 and beyond are kinda safe to use
+unsigned int localPort = 50000; 
+
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+char  ReplyBuffer[] = "acknowledged"; 
+
+EthernetUDP Udp;
+
 
 void setup() {
   Serial.begin(9600);
   while(!Serial){
     ;
   }
-  //SETUP
+  //<setup>
   Serial.println("Trying to obtain IP from DHCP...");
   if (Ethernet.begin(mac) == 0) {
     if (Ethernet.hardwareStatus() == EthernetNoHardware) {
@@ -34,14 +42,45 @@ void setup() {
     Serial.println("Failed to obtain IP from DHCP, setting default values.");
     Ethernet.begin(mac, ip);
   }
-  
 
   Serial.print("Arduino adress: ");
   Serial.println(Ethernet.localIP());
+  ///<setup>
 
+  Udp.begin(localPort);
+
+  
 }
 
 void loop() {
+  int packetSize = Udp.parsePacket();
+  if(packetSize)
+  {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remote = Udp.remoteIP();
+    for (int i =0; i < 4; i++)
+    {
+      Serial.print(remote[i], DEC);
+      if (i < 3)
+      {
+        Serial.print(".");
+      }
+    }
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
 
- Ethernet.maintain();
+    // read the packet into packetBufffer
+    Udp.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
+
+    // send a reply, to the IP address and port that sent us the packet we received
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
+  }
+  delay(10);
+  Ethernet.maintain();
 }
