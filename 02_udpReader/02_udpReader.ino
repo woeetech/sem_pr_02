d2afa95c00657dec2bd4cabb05465b9683d1adf9
woeetech <vlukas@hotmail.cz>
2020-10-11 18:36:28 +0200

    had to start from 'skratch' - DHCP wouldn't work and other errors appeared

--- 02_udpReader/02_udpReader.ino
+++ 02_udpReader/02_udpReader.ino
@@ -1,54 +1,47 @@
-
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
+//made-up MAC address
+byte mac[] = {
+  0x00, 0xAA, 0xBB, 0xCD, 0xDE, 0x02
+};
 
-
-byte mac[] = {  
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168,2,100);
-
-unsigned int localPort = 50000;
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
-char  ReplyBuffer[] = "acknowledged"; 
-
-EthernetUDP Udp;
-
+//if DHCP fails:
+IPAddress ip(192, 168, 2, 100);
+IPAddress myDns(192, 168, 2, 1);
+IPAddress gateway(192, 168, 2, 1);
+IPAddress subnet(255, 255, 255,0);
 
 
 void setup() {
- 
+  Serial.begin(9600);
+  while(!Serial){
+    ;
+  }
+  //SETUP
   Serial.println("Trying to obtain IP from DHCP...");
   if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed!");
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield not found!");
+      while(true) {
+        delay(1); //basically do nothing, we're done, end of story...
+      }  
+    }  
+    if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable not connected!"); //never got this message, even with cable unplugged
+    }
+    Serial.println("Failed to obtain IP from DHCP, setting default values.");
     Ethernet.begin(mac, ip);
   }
-  Serial.begin(9600);
+  
+
+  Serial.print("Arduino adress: ");
+  Serial.println(Ethernet.localIP());
+
 }
 
 void loop() {
- int packetSize = Udp.parsePacket();
- if (packetSize) {
-  Serial.print("Received packet of size ");
-    Serial.println(packetSize);
-    Serial.print("From ");
-    IPAddress remote = Udp.remoteIP();
-    for (int i =0; i < 4; i++)
-    {
-      Serial.print(remote[i], DEC);
-      if (i < 3)
-      {
-        Serial.print(".");
-      }
-    }
-    Serial.print(", port ");
-    Serial.println(Udp.remotePort());
 
-    udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-    Serial.println("Contents:");
-    Serial.println(packetBuffer);
- }
-  delay(10);
-  Ethernet.maintain();
+ Ethernet.maintain();
 }
